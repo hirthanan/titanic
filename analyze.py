8cf7ec0b30d632b11797107527e5c219f8e363af
Hirthanan Subenderan <Hirthanan_97@hotmail.com>
2018-01-31 22:14:04 -0500

    cleaned data and trained random forest model

--- analyze.py
+++ analyze.py
@@ -1,15 +1,42 @@
+# comma seperated values library to parse titanic data file
 import csv
 
-def analyzer(data):
-    for row in data:
-        print row["PassengerId"]
+# RandomForestClassifier used to predict survival using sample data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-def csv_reader(fileObj):
-    reader = csv.DictReader(fileObj, delimiter=',')
-    return reader
+# Load numpy
+import numpy as np
+
+# Load pandas
+import pandas as pd
+
+# Set randomizer seed
+np.random.seed(0)
+
+def analyzer(data):
+    clf = RandomForestClassifier()
+
+
+
+def handle_missing_vals (df):
+    del df['Age']
+    del df['Cabin']
+    return df
 
 if __name__ == "__main__":
-    ''' format train.csv data for python to interpret '''
-    with open("train.csv") as f:
-        data = csv_reader(f)
-        analyzer(data)
+    test_df = pd.read_csv('test.csv')
+    train_df = pd.read_csv('train.csv')
+
+    print train_df['Survived']
+    # remove columns with missing data
+    train_df = handle_missing_vals(train_df)
+
+    train_x = train_df.drop('Survived', axis=1)
+    train_y = train_df['Survived']
+
+
+    analyzer(train_df)
+
+
+
