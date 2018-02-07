19269b42f3c5764d8c917746ea60c80926c997a9
Hirthanan Subenderan <Hirthanan_97@hotmail.com>
2018-02-06 20:00:14 -0500

    complete analysis of data to predict titanic survivors

--- analyze.py
+++ analyze.py
@@ -28,13 +28,31 @@ def encode_gender(df):
             df.set_value(idx, 'Sex', 1)
     return df
 
+def encode_embarked(df):
+    for idx, row in df.iterrows():
+        if row['Embarked'] == 'Q':
+            df.set_value(idx, 'Embarked', 0)
+        elif row['Embarked'] == 'S':
+            df.set_value(idx, 'Embarked', 1)
+        else:
+            df.set_value(idx, 'Embarked', 2)
+    return df
+
+def handle_missing_data(df):
+    age_median = df['Age'].median()
+    fare_median = df['Fare'].median()
+    df['Age'].fillna(age_median, inplace=True)
+    df['Fare'].fillna(fare_median, inplace=True)
+    return df
+
 def clean_data (df):
     del df['Cabin']
     del df['Ticket']
     del df['Name']
 
     df = encode_gender(df)
-    print df.describe()
+    df = encode_embarked(df)
+    df = handle_missing_data(df)
     return df
 
 if __name__ == "__main__":
@@ -43,15 +61,16 @@ if __name__ == "__main__":
 
     # clean data and encode strings to numerical values
 
-    # comment for now test_df = clean_data(test_df)
+    test_df = clean_data(test_df)
     train_df = clean_data(train_df)
-
-    #print train_df
 
     test_x = test_df
 
     train_x = train_df.drop('Survived', axis=1)
     train_y = train_df['Survived']
 
-    # trained_rforest = analyzer(train_x, train_y)
-    #print trained_rforest
+    rforest_model = analyzer(train_x, train_y)
+    predictions = rforest_model.predict(test_x)
+    prediction = pd.DataFrame(range(892, 1310), columns=['PassengerId'])
+    prediction['Survived'] = predictions
+    prediction.to_csv('prediction.csv', index=False)
